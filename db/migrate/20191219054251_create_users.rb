d8ca7230ff9151aa9a32cd47a6ffc5af6fc00956
@Junk8383 <sksk14@icloud.com>
2019-12-19 16:42:22 +0900

    add_options

--- db/migrate/20191219054251_create_users.rb
+++ db/migrate/20191219054251_create_users.rb
@@ -6,13 +6,13 @@ class CreateUsers < ActiveRecord::Migration[5.0]
       t.string :first_name      , null: false
       t.string :family_name_kana, null: false
       t.string :first_name_kana , null: false
-      t.integer :tell
-      t.string :birth_year
-      t.string :birth_month
-      t.string :birth_day
-      t.integer :sales
+      t.integer :tell           , null: false, unique: true
+      t.string :birth_year      , null: false
+      t.string :birth_month     , null: false
+      t.string :birth_day       , null: false
+      t.integer :sales          , null: false
       t.text :image
-      t.datetime :deleted_at
+      t.datetime :deleted_at    ,null: false
       t.timestamps
     end
   end
