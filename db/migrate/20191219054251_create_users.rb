8e796de200d216e99162b5d09da5b0f21312d3ab
@Junk8383 <sksk14@icloud.com>
2019-12-19 16:35:12 +0900

    add_user_table

--- db/migrate/20191219054251_create_users.rb
+++ db/migrate/20191219054251_create_users.rb
@@ -1,7 +1,18 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-
+      t.string :nickname        , null: false
+      t.string :family_name     , null: false
+      t.string :first_name      , null: false
+      t.string :family_name_kana, null: false
+      t.string :first_name_kana , null: false
+      t.integer :tell
+      t.string :birth_year
+      t.string :birth_month
+      t.string :birth_day
+      t.integer :sales
+      t.text :image
+      t.datetime :deleted_at
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191219054251) do
-
-  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
