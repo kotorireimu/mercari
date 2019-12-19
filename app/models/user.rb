9f01b7b84bbd4186881250d41af6472fa90e3196
@Junk8383 <sksk14@icloud.com>
2019-12-19 19:32:14 +0900

    add_validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :tell, :birth_year, :birth_month, :birth_day, :delete_at, presence: true
+  validates :tell, unique: true
 end
